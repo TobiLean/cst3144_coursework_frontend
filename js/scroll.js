c2aee2f95b3d789a7b76c80458cb03e453581bed
TobiLean <Tobiloba.tsl@gmail.com>
2024-11-19 15:01:23 +0400

    Updated navbar scroll function, made aesthetic changes to frontendm, added functionality for front-end

--- js/scroll.js
+++ js/scroll.js
@@ -1,6 +1,6 @@
 window.addEventListener("scroll", () => {
     const navbar = document.getElementById("navbar");
-    if (window.scrollY > 50) {
+    if (window.scrollY > 380) {
         navbar.classList.add("navbar_shadow_class")
     } else {
         navbar.classList.remove("navbar_shadow_class")
--- js/vue.js
+++ js/vue.js
@@ -4,10 +4,24 @@ const app = new Vue({
     lessons: [],
     cart: [],
     showLessons: true,
+    page: 'home',
+    sortOptions: {
+      ascendingByPrice: false,
+      descendingByPrice: false,
+    }
   },
   methods: {
-    goToCheckOut () {
+    goToCheckOut() {
+      this.page = 'checkout';
       this.showLessons = !this.showLessons;
+      let backHomeBtn = document.getElementsByClassName('checkout_btn');
+      backHomeBtn[0].innerText = 'Back';
+    },
+    goToHome() {
+      this.page = 'home';
+      this.showLessons = !this.showLessons;
+      let backHomeBtn = document.getElementsByClassName('back_home_btn');
+      backHomeBtn[0].innerText = 'Checkout';
     },
     async getLessons() {
       try {
@@ -15,17 +29,91 @@ const app = new Vue({
           method: 'GET',
         });
         let data = await response.json();
+        let sorted
+        console.log("def", this.sortOptions.ascendingByPrice)
+
         console.log("Got lessons successfully: ", data);
-        this.lessons = data
+        // this.lessons = data
+        if (this.sortOptions.ascendingByPrice === true) {
+          console.log("func", this.sortOptions.ascendingByPrice)
+          sorted = data.toSorted((a, b) => a.price - b.price);
+          this.lessons = sorted
+        } else {
+          this.lessons = data
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    },
+    async postOrder() {
+      try {
+        const form = document.getElementById("orderForm");
+        const formData = new FormData(form);
+
+        const jsonData = {};
+        jsonData.cartItems = []
+
+        // Getting each form item and adding it to json object
+        formData.forEach((value, key) => {
+          jsonData[key] = value;
+        });
+
+        if (this.cart.length > 0) {
+          this.cart.forEach((item) => {
+            jsonData.cartItems.push(item);
+          })
+        }
+
+        const response = await fetch("http://localhost:8090/order", {
+          method: 'POST',
+          body: JSON.stringify(jsonData),
+          headers: {
+            'Content-Type': 'application/json',
+          }
+        });
+        let order = await response.json();
+        console.log("Test order successfully: ", order);
       } catch (err) {
         console.error(err);
       }
     },
     addToCart(index) {
-      this.cart.push(this.lessons[index].id);
-
-      console.log(this.lessons[index].id);
+      this.cart.push(this.lessons[index]);
+      console.log(this.lessons[index].subject, " has been added to cart!");
     },
+    removeFromCart(id) {
+      let index = this.cart.findIndex(lesson => lesson.id === id);
+      this.cart.splice(index, 1);
+      console.log(this.lessons[index].subject + " at index " + index + " of cart has been removed from cart!");
+    },
+    comparePrice(a, b) {
+      if (a.price < b.price) {
+        return -1
+      } else if (a.price > b.price) {
+        return 1
+      }
+
+      return 0
+    },
+    sortLessons() {
+      let sorted;
+
+      if (this.sortOptions.ascendingByPrice === true) {
+        console.log("func", this.sortOptions.ascendingByPrice)
+        sorted = this.lessons.toSorted((a, b) => a.price - b.price);
+        this.lessons = sorted
+      } else if (this.sortOptions.descendingByPrice === true) {
+        console.log("func", this.sortOptions.descendingByPrice)
+        sorted = this.lessons.toSorted((a, b) => b.price - a.price);
+        this.lessons = sorted
+      }
+    },
+    updateAllSortingOptions() {
+      Object.keys(this.sortOptions).forEach(key => {
+        this.sortOptions[key] = false;
+        console.log(this.sortOptions[key])
+      });
+    }
   },
   mounted() {
     this.getLessons();
